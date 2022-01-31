89929b8cd903ce6a210285fe9eb1ef53a9926eca
herald-of-spring <anhnguye@usc.edu>
2022-01-31 02:57:56 -0800

    1st version, complete with commentary.

--- src/FlightMap.java
+++ src/FlightMap.java
@@ -4,14 +4,51 @@ public class FlightMap {
 	private Map<Character, Integer> destMap;
 	private Character name;
 	
-	public FlightMap(Character c) {
-		name = c;
-		destMap = new HashMap();
+	/**
+	 * Constructor to create a node equivalent.
+	 * 
+	 * @param name name of location, character type
+	 */
+	public FlightMap(Character name) {
+		this.name = name;
+		destMap = new HashMap<Character, Integer>();
 	}
+	
+	/**
+	 * Connects each node together (one-directionally) with their associated costs.
+	 * 
+	 * @param dest destination name, character type
+	 * @param cost cost of traversing this path, integer type
+	 */
 	public void add(Character dest, Integer cost) {
-		
+		destMap.put(dest, cost);
 	}
-	public Set<Character> list() {
-		
+	
+	/**
+	 * Provides a list for graph traversal.
+	 * 
+	 * @return A set of characters listing available next destinations
+	 */
+	public Set<Character> next() {
+		return destMap.keySet();
+	}
+	
+	/**
+	 * Returns the cost to add into itinerary.
+	 * 
+	 * @param dest destination name, must be connected (i.e found in next()), character type
+	 * @return The cost of traversing this edge
+	 */
+	public Integer cost(Character dest) {
+		return destMap.get(dest);
+	}
+	
+	/**
+	 * Gets name
+	 * 
+	 * @return Name of location
+	 */
+	public Character getName() {
+		return name;
 	}
 }
--- src/FlightMapTest.java
+++ src/FlightMapTest.java
@@ -1,10 +1,15 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import java.util.*;
+
+import org.junit.*;
 
 class FlightMapTest {
-
+	private FlightMap a = new FlightMap('A');
+	private FlightMap b = new FlightMap('B');
+	private FlightMap c = new FlightMap('C');
+	private FlightMap d = new FlightMap('D');
+	private FlightMap e = new FlightMap('E');
 	/**
 	 * Environment setup.
 	 * <p>
@@ -18,13 +23,8 @@ class FlightMapTest {
 	 * 
 	 * @throws java.lang.Exception
 	 */
-	@BeforeEach
-	void setUp() throws Exception {
-		FlightMap a = new FlightMap('A');
-		FlightMap b = new FlightMap('B');
-		FlightMap c = new FlightMap('C');
-		FlightMap d = new FlightMap('D');
-		FlightMap e = new FlightMap('E');
+	@Before
+	public void setUp() throws Exception {
 		a.add('B', 200);
 		a.add('C', 400);
 		b.add('D', 1000);
@@ -34,10 +34,24 @@ class FlightMapTest {
 		e.add('D', 900);
 	}
 
+	/**
+	 * Tests node connectivity (on A).
+	 */
 	@Test
-	void listTest() {
-		
-		assertEquals("Not yet implemented");
+	public void nextTest() {
+		Set<Character> ans = new HashSet<Character>();
+		ans.add('B');
+		ans.add('C');
+		assertEquals(ans, a.next());
 	}
-
+	
+	/**
+	 * Tests itinerary cost (from A to C, from A to D).
+	 * Overhead algorithm of BFS, not taking into account costs.
+	 */
+	@Test
+	public void costTest() {
+		assertEquals(400, a.cost('C'));
+		assertEquals(200+1000, a.cost('B')+b.cost('D'));
+	}
 }
