016dee5c33fb8d93f44b5c71bb66f69ef52338f2
herald-of-spring <anhnguye@usc.edu>
2022-01-31 02:22:13 -0800

    'add' and 'listing' with associated 'listTest'

--- src/FlightMap.java
+++ src/FlightMap.java
@@ -2,5 +2,16 @@ import java.util.*;
 
 public class FlightMap {
 	private Map<Character, Integer> destMap;
+	private Character name;
 	
+	public FlightMap(Character c) {
+		name = c;
+		destMap = new HashMap();
+	}
+	public void add(Character dest, Integer cost) {
+		
+	}
+	public Set<Character> list() {
+		
+	}
 }
--- src/FlightMapTest.java
+++ src/FlightMapTest.java
@@ -7,15 +7,37 @@ class FlightMapTest {
 
 	/**
 	 * Environment setup.
+	 * <p>
+	 * ...A<-E
+	 * <p>
+	 * ../|^.|
+	 * <p>
+	 * .v.v.\v
+	 * <p>
+	 * C->B->D
+	 * 
 	 * @throws java.lang.Exception
 	 */
 	@BeforeEach
 	void setUp() throws Exception {
+		FlightMap a = new FlightMap('A');
+		FlightMap b = new FlightMap('B');
+		FlightMap c = new FlightMap('C');
+		FlightMap d = new FlightMap('D');
+		FlightMap e = new FlightMap('E');
+		a.add('B', 200);
+		a.add('C', 400);
+		b.add('D', 1000);
+		c.add('B', 500);
+		d.add('A', 700);
+		e.add('A', 100);
+		e.add('D', 900);
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void listTest() {
+		
+		assertEquals("Not yet implemented");
 	}
 
 }
