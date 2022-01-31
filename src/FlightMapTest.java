73f56424c4e94bd4960dbb9cda8f9c3bae59445d
herald-of-spring <anhnguye@usc.edu>
2022-01-31 01:49:19 -0800

    Ant build and JUnit class created

---
+++ src/FlightMapTest.java
@@ -0,0 +1,21 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class FlightMapTest {
+
+	/**
+	 * Environment setup.
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
